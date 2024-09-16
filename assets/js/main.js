af36186752e5cbe4b70e231f30bebf8cac82d6d6
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-09-16 16:15:57 -0500

    Added hero imagery and created logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,10 @@
       // === logo change
       if (ud_header.classList.contains("sticky")) {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo.svg"
+          "assets/images/logo/translate-logo-colored.svg"
       } else {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo-white.svg"
+          "assets/images/logo/translate-logo-colored.svg"
       }
     }
 
